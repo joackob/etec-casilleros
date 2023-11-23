6b1e19f31d4a505874550d94a3ef32371b328984
Carlos_Liz <131411538+carlosyLizbeth@users.noreply.github.com>
2023-11-23 22:59:02 +0000

    Update 3 files

--- pages/[....nextauth]/route.tsx
+++ pages/[....nextauth]/route.tsx
@@ -6,10 +6,9 @@ const handler = NextAuth ({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-
         },
         ),
     ],
 });
 
-export {handler as GET, as }
+export {handler as GET, handler as POST};
